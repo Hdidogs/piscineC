14010467c3acb753bc27650cedc8c9af9c8fc8f0
Hdidogs <a.rohee@lprs.fr>
2025-09-25 17:08:15 +0200

    Feat: ship struct

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+typedef struct {
+    unsigned int x;
+    unsigned int y;
+    unsigned short live;
+    unsigned short size;
+} Ship;
+
 int main(void) {
     printf("Hello, World!\n");
     return 0;
